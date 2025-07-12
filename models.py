81927e72ec7f3a118ddd1976be2a2fc5e8d140f0
Hosea <karanja.mungai23@gmail.com>
2025-07-12 20:22:13 +0300

    establishes many to many relationship riles and permissions tables

--- models.py
+++ models.py
@@ -113,7 +113,7 @@ class Image (db.Model, SerializerMixin):
     data=db.Column(db.LargeBinary)
     finding_id = db.Column(db.Integer, db.ForeignKey('findings.id'),nullable=True)
     uploaded_at = db.Column(db.DateTime, default=datetime.now)
-    
+   
 class Users(db.Model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -121,18 +121,34 @@ class Users(db.Model, SerializerMixin):
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    roles = db.relationship('Roles', backref='users')
-    
+
+    role = db.relationship('Roles', back_populates='users')
+     
+role_permissions = db.Table(
+    'role_permissions',
+    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'), primary_key=True),
+    db.Column('permission_id', db.Integer, db.ForeignKey('permissions.id'), primary_key=True)
+)
 
 class Roles(db.Model, SerializerMixin):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    users = db.relationship('Users', backref='roles')
-    
-    
+
+    permissions = db.relationship(
+        'Permissions',
+        secondary=role_permissions,
+        back_populates='roles'
+    )       
+    users = db.relationship('Users', back_populates='role')
 class Permissions(db.Model, SerializerMixin):
     __tablename__ = 'permissions'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    roles = db.relationship('Roles', backref='permissions')
+
+    roles = db.relationship(
+        'Roles',
+        secondary=role_permissions,
+        back_populates='permissions'
+    )
+   
